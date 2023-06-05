f2d7cbcccb61e58d7cc31f9387f59ae304b913b9
rahmanshaukat35 <rahmanshaukat35@gmail.com>
2023-06-05 23:26:43 +0500

    new

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,6 +1,37 @@
-import { NextResponse } from 'next/server';
+import { db } from '@vercel/postgres';
+import { NextRequest, NextResponse } from 'next/server';
 
-// import {db} from '@vercel/postgres'
-export function GET(request: Request) {
-	return NextResponse.json({ Message: 'you called this api' });
+export async function GET(request: NextRequest) {
+	const client = await db.connect();
+	try {
+		await client.sql`CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255));`;
+		const res = await client.sql`SELECT * FROM Todos`;
+		console.log(res.rows.find((item) => item.id === 1));
+
+		return NextResponse.json({ data: res });
+	} catch (err) {
+		console.log(err);
+
+		return NextResponse.json({ message: 'something went wrong' });
+	}
+}
+
+export async function POST(request: NextRequest) {
+	const client = await db.connect();
+	const req = await request.json();
+	try {
+		if (req.task) {
+			const res =
+				await client.sql`INSERT INTO Todos (Task) VALUES (${req.task});`;
+			console.log(res);
+
+			return NextResponse.json({ Message: 'you added data' });
+		} else {
+			throw new Error('task field is required');
+		}
+	} catch (error) {
+		return NextResponse.json({
+			message: (error as { message: string }).message,
+		});
+	}
 }
